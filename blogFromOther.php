0234e5086c5575763fd24731c70b3ac58110c2b2
Osher Boudara <osherboudara99@gmail.com>
2021-08-09 14:32:50 -0700

    change view other query

--- blogFromOther.php
+++ blogFromOther.php
@@ -42,13 +42,12 @@ include("db.php");
 
 session_start();
 
-$query_viewblogs = $conn->prepare("SELECT subject, description, pdate, GROUP_CONCAT(tag SEPARATOR ',') AS tags, username FROM Users 
+$query_viewblogs = $conn->query("SELECT subject, description, pdate, GROUP_CONCAT(tag SEPARATOR ',') AS tags, username FROM Users 
 INNER JOIN blogs ON Users.userid = blogs.userid 
 INNER JOIN blogstags ON blogs.blogid = blogstags.blogid 
-WHERE username != 
+WHERE username != '".$_SESSION['username']."'
 GROUP BY blogstags.blogid");
 
-$query_viewblogs->bind_param("s", $_SESSION['username']);
 $result = $query_viewblogs->execute();
 
 ?>
