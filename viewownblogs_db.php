d647259467d5e50f48ce0bbffc9e4b6a088267fd
Osher Boudara <osherboudara99@gmail.com>
2021-08-09 14:55:15 -0700

    sql own blogs updated

--- viewownblogs_db.php
+++ viewownblogs_db.php
@@ -3,14 +3,13 @@ include("db.php");
 
 session_start();
 
-$query_viewownblogs = $conn->prepare("SELECT subject, description, pdate, GROUP_CONCAT(tag SEPARATOR ',') AS tags, username FROM Users 
+$query_viewownblogs = $conn->query("SELECT subject, description, pdate, GROUP_CONCAT(tag SEPARATOR ',') AS tags, username FROM Users 
 INNER JOIN blogs ON Users.userid = blogs.userid 
 INNER JOIN blogstags ON blogs.blogid = blogstags.blogid 
-WHERE username = ?
-GROUP BY blogstags.blogid");
+WHERE username = '".$_SESSION['username']."'
+GROUP BY blogstags.blogid
+ORDER BY pdate DESC");
 
-$query_viewownblogs->bind_param("s", $_SESSION['username']);
-$result = $query_viewownblogs->execute();
 
 
 
