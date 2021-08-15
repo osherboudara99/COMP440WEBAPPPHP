930a01f6e8699b3af2d6f153f3d7b091b1e9c922
Osher Boudara <osherboudara99@gmail.com>
2021-08-15 12:46:50 -0700

    remainder queries complete

--- phase3_sql.php
+++ phase3_sql.php
@@ -4,26 +4,51 @@ include ("db.php");
 
 session_start();
 
-$sql_viewblogs_poscomments1 = "SELECT subject, blogs.description, pdate, GROUP_CONCAT(tag SEPARATOR ',') AS tags, username FROM Users 
+$sql_viewblogs_poscomments1 = "SELECT subject, blogs.description, pdate, GROUP_CONCAT(DISTINCT tag SEPARATOR ',') AS tags, username FROM Users 
 INNER JOIN blogs ON Users.userid = blogs.userid 
 INNER JOIN blogstags ON blogs.blogid = blogstags.blogid 
 INNER JOIN comments ON comments.authorid = blogs.userid
 WHERE username = '".$_SESSION['username']."' AND sentiment LIKE '%positive%'
 GROUP BY blogstags.blogid
-ORDER BY pdate DESC";
+ORDER BY pdate DESC"; //session username chnaged to user input
 
 $sql_viewpositivecomments1 = "SELECT username, sentiment, comments.description AS comment_desc, cdate FROM comments
 INNER JOIN Users
 ON comments.authorid = Users.userid
 INNER JOIN blogs
 ON blogs.blogid = comments.blogid
-WHERE blogs.subject = '".$row["subject"]."' AND sentiment LIKE '%positive%'";
+WHERE blogs.subject = '".$row["subject"]."' AND sentiment LIKE '%positive%'"; //must retrieve subject from above query
 
 
 $sql_mostblogsdate2 = "SELECT username, COUNT(blogid) AS blog_amount FROM Users
 INNER JOIN blogs
 ON blogs.userid = Users.userid
-WHERE pdate = '2020-10-10'";
+WHERE pdate = '2020-10-10'"; //chnage date to user input
+
+$sql_followedbytwousers3 = "SELECT a.username AS followed FROM follows
+INNER JOIN Users AS a
+ON a.userid = leaderid
+INNER JOIN Users AS b
+ON b.userid = followerid
+WHERE b.username IN ('catlover', 'scooby') 
+GROUP BY a.username 
+HAVING COUNT(*) = 2"; //chnage catlover to variable and scooby
+
+
+$sql_tagssearch4 = "SELECT subject, blogs.description, pdate, GROUP_CONCAT(DISTINCT tag SEPARATOR ',') AS tags, username FROM Users 
+INNER JOIN blogs ON Users.userid = blogs.userid 
+INNER JOIN blogstags ON blogs.blogid = blogstags.blogid 
+INNER JOIN comments ON comments.authorid = blogs.userid
+WHERE tag LIKE '%dogs%'
+GROUP BY blogstags.blogid
+ORDER BY pdate DESC"; //change tag like parameter to variable
+
+$sql_nocommentsblogs5 = "SELECT subject, blogs.description, pdate, GROUP_CONCAT(DISTINCT tag SEPARATOR ',') AS tags, username FROM Users 
+INNER JOIN blogs ON Users.userid = blogs.userid 
+INNER JOIN blogstags ON blogs.blogid = blogstags.blogid 
+WHERE blogs.blogid NOT IN (SELECT DISTINCT comments.blogid FROM comments)
+GROUP BY blogstags.blogid
+ORDER BY pdate DESC";
 
 
 
